1d049e0ca19a55f04e36c245049839f7de6e121f
Ethan Isola <isolaethan@gmail.com>
2022-03-07 11:59:02 -0600

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/movies" do
+    Movie.all.to_json
   end
+
+  get '/movies/:id' do
+    Movie.find(params[:id]).to_json
+  end
+
+  post '/movies' do 
+    Movie.create(
+    title: params[:title],
+    release_date: params[:release_date],
+    cast: params[:cast],
+    director: params[:director],
+    producer: params[:producer],
+    image_URL: params[:image_URL]
+  )
+  end
+
 
 end
